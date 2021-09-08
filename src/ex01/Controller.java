e4b108ac43a1fa876c93a97d5c1f14eadb6f1a36
Choi.SY <sollly0829@gmail.com>
2021-09-08 10:59:46 +0900

    fx07.ex01 chart(pie,bar,area) 기본 예시

--- src/ex01/Controller.java
+++ src/ex01/Controller.java
@@ -3,10 +3,57 @@ package ex01;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 
 public class Controller implements Initializable  {
 
+	Parent root;
+	PieChart pieChart;
+	BarChart barChart;
+	AreaChart areaChart;
+	
+	public void setRoot(Parent root) {
+		this.root=root;
+		pieChart = (PieChart)root.lookup("#pieChart");
+		barChart = (BarChart)root.lookup("#barChart");
+		areaChart = (AreaChart)root.lookup("#areaChart");
+		chart();
+	}
+	
+	public void chart() {
+	
+		pieChart.setData(FXCollections.observableArrayList(
+			new PieChart.Data("AWT", 10),new PieChart.Data("Swing", 30),
+			new PieChart.Data("JacaFX", 30),new PieChart.Data("Java", 30)));
+		
+		XYChart.Series series1 = new XYChart.Series();
+		series1.setName("남자");
+		series1.setData(FXCollections.observableArrayList(
+				new XYChart.Data("2016",70), new XYChart.Data("2017",40),
+				new XYChart.Data("2018",60), new XYChart.Data("2019",90)));
+		barChart.getData().add(series1);
+		
+		XYChart.Series series2 = new XYChart.Series();
+		series2.setName("여자");
+		series2.setData(FXCollections.observableArrayList(
+				new XYChart.Data("2016",80), new XYChart.Data("2017",60),
+				new XYChart.Data("2018",100), new XYChart.Data("2019",80)));
+		barChart.getData().add(series2);
+		
+		XYChart.Series series3 = new XYChart.Series();
+		series3.setName("평균 온도");
+		series3.setData(FXCollections.observableArrayList(
+				new XYChart.Data("2016",20), new XYChart.Data("2017",22),
+				new XYChart.Data("2018",23), new XYChart.Data("2019",21)));
+		areaChart.getData().add(series3);
+	}
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
--- src/ex01/MainClass.java
+++ src/ex01/MainClass.java
@@ -1,6 +1,9 @@
 package ex01;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainClass extends Application {
@@ -11,6 +14,16 @@ public class MainClass extends Application {
 
 	@Override
 	public void start(Stage arg0) throws Exception {
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("eventTest.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		
+		Controller ctl = loader.getController();
+		ctl.setRoot(root);
+		
+		arg0.setScene(scene);
+		arg0.setTitle("Naver Mainpage");
 		arg0.show();
 		
 	}
